332b4d908e7ee3e37e709e5be9e4f8a993bb6f6e
Admin <sorokivskyi777@gmail.com>
2023-02-28 18:28:44 +0200

    lesson1

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join('test'), (err) => {
-    console.log(err);
-});
+// fs.mkdir(path.join('test'), (err) => {
+//     console.log(err);
+// });
+
+// for (let i = 1; i < 6; i++) {
+//     fs.mkdir(path.join('test', `dir${i}`), (err) => {
+//         console.log(err);
+//     });
+//
+//     fs.writeFile(path.join('test', `myFile${i}.txt`), `DATA${i}`, (err) => {
+//         console.log(err);
+//     });
+//
+// }
+
+// fs.readdir(path.join('test'), {withFileTypes: true}, (err, files) => {
+//     console.log(err);
+//
+//     files.forEach(file => {
+//         if (file.isFile()) {
+//             console.log(`FILE : ${file.name}`)
+//         }else {
+//             console.log(`FOLDER : ${file.name}`)
+//         }
+//     });
+//
+// });
