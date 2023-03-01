54fa8276ee266eef3e00b0c6b619ceb312a19b59
Admin <sorokivskyi777@gmail.com>
2023-03-01 11:11:46 +0200

    leaning from hw_resolve1 with Maks

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-let fs = require('fs');
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+const worker = async () => {
+    const fileNames = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt'];
+    const folderNames = ['folder1', 'folder2', 'folder3', 'folder4'];
+
+    for (const folderName of folderNames) {
+        await fs.
+    }
+
+};
+
+worker();
