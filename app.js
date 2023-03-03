7c16797ad63f679feccd591d61e7cc1ec290d9c0
Admin <sorokivskyi777@gmail.com>
2023-03-03 18:55:58 +0200

    learn lesson 2

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 const express = require('express');
-const {json} = require("express");
+const fsService = require('./fs.service.js');
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const PORT = 5100;
 
 app.listen(PORT, () => {
-    console.log('server has started');
+    console.log('server has started on port 5100');
 });
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
 
-    console.log('USERS ENDPOINT')
+    const users = await fsService.reader();
+    res.json(users);
+
+});
+
+app.post('/users', async (req, res) => {
+
+    const {name, age, gender} = req.body;
+
+    const users = await fsService.reader();
+
+    const newUser = {
+        id: users[users.length - 1]?.id + 1 || 1,
+        name,
+        age,
+        gender,
+    };
+
+    users.push(newUser);
+
+
 
 });
